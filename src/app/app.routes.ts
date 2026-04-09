64d759b7c461918f1b138dcc951f2e1c88c24324
Giovani Ocan <giovaniocan@Giovanis-MacBook-Pro.local>
2026-04-09 15:40:56 -0300

    feat: added properly routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { OrdersListComponent } from './features/orders/order-list/orders-list.component';
+import { OrderDetailComponent } from './features/orders/order-details/order-detail.component';
 
-export const routes: Routes = [];
+// Equivalente ao <Routes> do React Router
+export const routes: Routes = [
+  { path: '', redirectTo: 'orders', pathMatch: 'full' },
+  { path: 'orders', component: OrdersListComponent },
+  { path: 'orders/:id', component: OrderDetailComponent },
+];
